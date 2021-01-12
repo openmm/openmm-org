f3b2b5b99d01a17162ab9f51884b2149fae2c0c5
Raimondas Galvelis <r.galvelis@acellera.com>
2021-01-12 21:05:17 +0100

    Fix code highligthing

--- home.vue
+++ home.vue
@@ -15,5 +15,32 @@
         </v-card>
       </v-col>
     </v-row>
+    <v-row>
+      <v-col cols="auto">
+        <v-card flat>
+          <v-card-title>
+            Use OpenMM from Python
+          </v-card-title>
+          <highlightjs language='python' code="from simtk.openmm.app import *
+from simtk.openmm import *
+from simtk.unit import *
+from sys import stdout
+
+pdb = PDBFile('input.pdb')
+forcefield = ForceField('amber99sb.xml', 'tip3p.xml')
+system = forcefield.createSystem(pdb.topology, nonbondedMethod=PME, nonbondedCutoff=1*nanometer, constraints=HBonds)
+integrator = LangevinIntegrator(300*kelvin, 1/picosecond, 0.002*picoseconds)
+simulation = Simulation(pdb.topology, system, integrator)
+simulation.context.setPositions(pdb.positions)
+simulation.minimizeEnergy()
+simulation.reporters.append(PDBReporter('output.pdb', 1000))
+simulation.reporters.append(StateDataReporter(stdout, 1000, step=True, potentialEnergy=True, temperature=True))
+simulation.step(10000)"></highlightjs>
+          <v-card-actions>
+            <v-btn>Script builder</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
