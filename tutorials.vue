19254f26a8d5a1d5159135c71fa91ad5928bd8fa
Raimondas Galvelis <r.galvelis@acellera.com>
2021-01-16 19:57:25 +0100

    Add partially the tutorial tab

---
+++ tutorials.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container>
+  </v-container>
+</template>
+
+<script>
+module.exports = {
+  mounted: function() {
+    fetch('tutorials/alchemical-free-energy/index.md').then((res) => {
+      res.text().then((str) => {
+        const el = document.createElement('div');
+        el.innerHTML = marked(str)
+        this.$el.appendChild(el);
+      })
+    })
+  }
+}
+</script>
