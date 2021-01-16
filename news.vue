966bfa6aeefdcafbbd03154217187d21260d7629
Raimondas Galvelis <r.galvelis@acellera.com>
2021-01-16 21:33:13 +0100

    Improve the news tab

---
+++ news.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container class="d-flex justify-center">
+    <a class="twitter-timeline" href="https://twitter.com/openmm_toolkit?ref_src=twsrc%5Etfw" width="600">Tweets by openmm_toolkit</a>
+  </v-container>
+</template>
+
+<script>
+module.exports = {
+  mounted: function() {
+    const script = document.createElement('script');
+    script.src = 'https://platform.twitter.com/widgets.js';
+    this.$el.appendChild(script)
+  }  
+}
+</script>
