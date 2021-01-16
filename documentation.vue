87edfcb382bd84cfbee3c601ce82c9641db766b6
Raimondas Galvelis <r.galvelis@acellera.com>
2021-01-16 01:27:14 +0100

    Add a docs tab

---
+++ documentation.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container class="d-flex flex-wrap justify-center">
+    <v-card href="http://docs.openmm.org/latest/userguide/" target="blank" class="ma-4" width="300" hover>
+      <v-card-title>User Guide</v-card-title>
+      <v-card-text>
+        Start here if it's your first time using OpenMM. These documents will get you up and running simulations in short order.
+      </v-card-text>
+    </v-card>
+    <v-card href="http://docs.openmm.org/latest/developerguide/" target="blank" class="ma-4" width="300" hover>
+      <v-card-title>Developer Guide</v-card-title>
+      <v-card-text>
+        This section provides in-depth information on the code-base and how to contribute to the development of OpenMM itself.
+      </v-card-text>
+    </v-card>
+    <v-card href="http://docs.openmm.org/latest/api-python/" target="blank" class="ma-4" width="300" hover>
+      <v-card-title>Python API</v-card-title>
+      <v-card-text>
+        The Python API provides information about the classes and methods available to Python developers.
+      </v-card-text>
+    </v-card>
+    <v-card href="http://docs.openmm.org/latest/api-c++/" target="blank" class="ma-4" width="300" hover>
+      <v-card-title>C++ API</v-card-title>
+      <v-card-text>
+        The C++ API provides information about OpenMM's C++ interface.
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
