07f62d8dae01a9c52a72fc4c2c4eed1f8573c58a
Raimondas Galvelis <r.galvelis@acellera.com>
2021-04-01 14:12:08 +0200

    Fix the scalling of the header

--- top.vue
+++ top.vue
@@ -1,17 +1,13 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col cols="4">
-        <v-img src="img/logos/Icon.svg" contain height="200px">
-      </v-col>
-      <v-col cols="8">
-        <v-card class="grey lighten-3" flat>
-          <v-card-title class="text-h2 font-weight-medium">OpenMM</v-card-title>
-          <v-card-text class="text-justify">
+  <v-container class="d-flex flex-wrap justify-space-around">
+    <v-card class="grey lighten-3" width="200px" flat>
+      <v-img src="img/logos/Icon.svg">
+    </v-card>
+    <v-card class="grey lighten-3" width="70%" flat>
+      <v-card-title class="text-h2 font-weight-medium">OpenMM</v-card-title>
+      <v-card-text class="text-justify">
             A high performance toolkit for molecular simulation. Use it as a library, or as an application. We include extensive language bindings for Python, C, C++, and even Fortran. The code is open source and actively maintained on Github, licensed under MIT and LGPL. Part of the <a href="https://omniamd.squarespace.com">Omnia</a> suite of tools for predictive biomolecular simulation.
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
+      </v-card-text>
+    </v-card>
   </v-container>
 </template>
