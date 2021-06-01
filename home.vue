dcfddf5138bd165f370ba24c042f3bd00ae928a6
peastman <peastman@stanford.edu>
2021-06-01 16:02:33 -0700

    Redesigned home page

--- home.vue
+++ home.vue
@@ -1,30 +1,56 @@
 <template>
   <div>
-    <v-container class="d-flex flex-column align-center">
-      <v-card class="my-12" width="600" flat>
-        <v-carousel width="600" height="375" cycle interval="5000" @change="changeSlideColor()">
-          <v-carousel-slide title="Custom Forces" :color="slideColor">
-            Want a custom force between two atoms? No problem. Write your force expressions in string format, and OpenMM will generate blazing fast code to do just that. No more hand-writing GPU kernels.
-          </v-carousel-slide>
-          <v-carousel-slide title="Highly Optimized" :color="slideColor">
-            OpenMM is optimized for the latest generation of compute hardware, including AMD (via OpenCL) and NVIDIA (via CUDA) GPUs. We also heavily optimize for CPUs using intrinsics.
-          </v-carousel-slide>
-          <v-carousel-slide title="Portable" :color="slideColor">
-            We strive to make our binaries as portable as possible. We've tested OpenMM on many flavors of Linux, OS X, and even Windows.
-          </v-carousel-slide>
-          <v-carousel-slide title="Easy Installation" :color="slideColor">
-            Install OpenMM and all its dependencies with Conda package manager. No manual compilation
-            from source required. 
-          </v-carousel-slide>
-        </v-carousel>
+    <v-container>
+      <v-card flat>
+        <v-card-title>Welcome to OpenMM</v-card-title>
+        <v-card-text>
+          A high-performance toolkit for molecular simulation. Use it as a library or as an application. We include extensive language bindings for Python, C, C++, and even Fortran. The code is open source and developed on GitHub, licensed under MIT and LGPL.
+        </v-card-text>
       </v-card>
-      <v-btn class="mt-6" href="https://colab.research.google.com/gist/raimis/1c83c413197410675e2c4271d9003f91" target="blank">
-        <v-icon left >mdi-play-circle-outline</v-icon>
-        Run in Colab
-      </v-btn>
-      <v-card class="my-6" width="1000">
-        <div id="tutorial-temporary"></div>
-        <iframe id="tutorial" class="mb-n2" frameborder="0" width="100%" height="6010" src=""></iframe>
+    </v-container>
+    <v-container class="d-flex flex-wrap justify-center">
+      <v-card v-on:click="httpVueLoader('benchmarks.vue')" class="ma-4" width="300" hover>
+        <v-card-title>Highly Optimized</v-card-title>
+        <v-card-text>
+          OpenMM is optimized for the latest generation of compute hardware, including NVIDIA, AMD, and Intel GPUs. We also heavily optimize for CPUs using intrinsics.
+        </v-card-text>
+      </v-card>
+      <v-card href="http://docs.openmm.org/latest/userguide/application.html#installing-openmm" class="ma-4" width="300" hover>
+        <v-card-title>Easy Installation</v-card-title>
+        <v-card-text>
+          Install OpenMM and all its dependencies with the Conda package manager. No manual compilation from source required. 
+        </v-card-text>
+      </v-card>
+      <v-card href="http://docs.openmm.org/latest/userguide/theory.html#custom-forces" class="ma-4" width="300" hover>
+        <v-card-title>Custom Forces</v-card-title>
+        <v-card-text>
+          Want a custom force between two atoms? No problem. Write your force expressions in string format, and OpenMM will generate blazing fast code to do just that. No more hand-writing GPU kernels.
+        </v-card-text>
+      </v-card>
+      <v-card href="http://docs.openmm.org/latest/userguide/application.html#the-openmm-setup-application" class="ma-4" width="300" hover>
+        <v-card-title>User Friendly</v-card-title>
+        <v-card-text>
+          OpenMM-Setup provide a simple UI to prepare input files, configure options, and run simulations.
+        </v-card-text>
+      </v-card>
+    </v-container>
+    <v-container>
+      <v-card flat>
+        <v-card-title>Installation</v-card-title>
+        <v-card-text>
+          OpenMM can be easily installed with Conda package manager:
+          <highlightjs class="py-4" language='bash' code="$ conda install -c conda-forge openmm"></highlightjs>
+          More details are available in the <a href="http://docs.openmm.org/latest/userguide/application.html#installing-openmm" target="blank">documentaion</a>.
+        </v-card-text>
+      </v-card>
+    </v-container>
+    <v-container>
+      <v-card flat>
+        <v-card-title>Example Python Script</v-card-title>
+        <v-card-text class="text--primary">
+          Use Python scripting to create your own simulation protocols in just a few lines of code.
+          <highlightjs class="my-4" language='python' :code="example"/>
+        </v-card-text>
       </v-card>
     </v-container>
   </div>
@@ -32,30 +58,11 @@
 
 <script>
 module.exports = {
-  components: {
-    'v-carousel-slide': httpVueLoader('slide.vue')
-  },
   data: function() { return { 
-    slideColor: '',
-    slideColorIndex: 0,
-    tutorialURL: 'https://gist.github.com/raimis/1c83c413197410675e2c4271d9003f91'
+    example: ''
   }},
-  mounted: function() {
-    postscribe('#tutorial-temporary', `<script src="${this.tutorialURL}.js"><\/script>`, {
-      done: () => {
-        let src = document.getElementsByClassName('render-viewer')[0].src
-        document.getElementById('tutorial').src = src
-        document.getElementById('tutorial-temporary').remove()
-        }
-    })
-  },
-  methods: {
-    changeSlideColor: function() {
-      const colors = ['deep-orange', 'light-green', 'light-blue', 'amber']
-      const offset = Math.floor(Math.random() * (colors.length - 1)) + 1
-      this.slideColorIndex = (this.slideColorIndex + offset) % colors.length
-      this.slideColor = colors[this.slideColorIndex]
-    }
+  created: function() {
+    fetch('example.py').then(res => res.text().then(str => this.example = str))
   }
 }
 </script>
--- top.vue
+++ top.vue
@@ -1,14 +1,12 @@
 <template>
   <v-container class="d-flex flex-wrap justify-space-around">
-    <v-card class="grey lighten-3" height="200px" width="200px" flat>
+    <v-card class="grey lighten-3" height="150px" width="150px" flat>
       <v-img src="img/logos/Icon.svg">
     </v-card>
     <v-card class="grey lighten-3" width="70%" flat>
       <v-card-title class="text-h2 font-weight-medium">OpenMM</v-card-title>
-      <v-card-text class="text-justify">
-            A high-performance toolkit for molecular simulation.
-            Use it as a library, or as an application. We include extensive language bindings for Python, C, C++, and even Fortran.
-            The code is open source and developed on GitHub, licensed under MIT and LGPL.
+      <v-card-text class="text-body-1">
+        High performance, customizable molecular simulation.
       </v-card-text>
     </v-card>
   </v-container>
