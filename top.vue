2386e2d6c1e467f11b27ce17079870bdce831ec9
peastman <peastman@stanford.edu>
2021-06-02 15:04:47 -0700

    Improvements to home page and header

--- top.vue
+++ top.vue
@@ -1,9 +1,9 @@
 <template>
-  <v-container class="d-flex flex-wrap justify-space-around">
-    <v-card class="grey lighten-3" height="150px" width="150px" flat>
+  <v-container class="d-flex flex-wrap space-around" fluid>
+    <v-card class="grey lighten-3" height="120px" width="120px" flat>
       <v-img src="img/logos/Icon.svg">
     </v-card>
-    <v-card class="grey lighten-3" width="70%" flat>
+    <v-card class="grey lighten-3" flat>
       <v-card-title class="text-h2 font-weight-medium">OpenMM</v-card-title>
       <v-card-text class="text-body-1">
         High performance, customizable molecular simulation.
