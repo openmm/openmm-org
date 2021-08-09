b24b63feb4230c2dcc964d17a5f688e3e4284916
Raimondas Galvelis <r.galvelis@acellera.com>
2021-08-09 15:28:24 +0200

    Uniform tile layout

--- components/home.vue
+++ components/home.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <v-container fluid>
+    <v-container>
       <v-card flat>
         <v-card-title>Welcome to OpenMM</v-card-title>
         <v-card-text>
@@ -8,33 +8,33 @@
         </v-card-text>
       </v-card>
     </v-container>
-    <v-container class="d-flex flex-wrap justify-center" fluid>
-      <v-card v-on:click="$router.push('/benchmarks')" class="ma-4" width="200" hover>
+    <v-container class="d-flex flex-wrap justify-center">
+      <v-card v-on:click="$router.push('/benchmarks')" class="ma-4" width="300" hover>
         <v-card-title>Highly Optimized</v-card-title>
         <v-card-text>
           Optimized for the latest generation of compute hardware, including NVIDIA, AMD, and Intel GPUs. We also heavily optimize for CPUs.
         </v-card-text>
       </v-card>
-      <v-card href="http://docs.openmm.org/latest/userguide/application.html#installing-openmm" class="ma-4" width="200" hover>
+      <v-card href="http://docs.openmm.org/latest/userguide/application.html#installing-openmm" class="ma-4" width="300" hover>
         <v-card-title>Easy Installation</v-card-title>
         <v-card-text>
           Install OpenMM and all its dependencies with the Conda package manager. No manual compilation from source required. 
         </v-card-text>
       </v-card>
-      <v-card href="http://docs.openmm.org/latest/userguide/theory.html#custom-forces" class="ma-4" width="200" hover>
+      <v-card href="http://docs.openmm.org/latest/userguide/theory.html#custom-forces" class="ma-4" width="300" hover>
         <v-card-title>Custom Forces</v-card-title>
         <v-card-text>
           Want a custom force between atoms? Write your force expression as a string and OpenMM will generate blazing fast code to compute it.
         </v-card-text>
       </v-card>
-      <v-card href="http://docs.openmm.org/latest/userguide/application.html#the-openmm-setup-application" class="ma-4" width="200" hover>
+      <v-card href="http://docs.openmm.org/latest/userguide/application.html#the-openmm-setup-application" class="ma-4" width="300" hover>
         <v-card-title>User Friendly</v-card-title>
         <v-card-text>
           OpenMM-Setup provides a simple UI to prepare input files, configure options, and run simulations.
         </v-card-text>
       </v-card>
     </v-container>
-    <v-container fluid>
+    <v-container>
       <v-card flat>
         <v-card-title>Installation</v-card-title>
         <v-card-text>
@@ -44,7 +44,7 @@
         </v-card-text>
       </v-card>
     </v-container>
-    <v-container fluid>
+    <v-container>
       <v-card flat>
         <v-card-title>Get the Source</v-card-title>
         <v-card-text>
@@ -53,7 +53,7 @@
         </v-card-text>
       </v-card>
     </v-container>
-    <v-container fluid>
+    <v-container>
       <v-card flat>
         <v-card-title>The Power of Python Scripting</v-card-title>
         <v-card-text class="text--primary">
