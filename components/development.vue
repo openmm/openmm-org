9a07cfc939ff684c5034155d5d8ea939ac14a3b4
Evan Pretti <pretti@stanford.edu>
2026-06-18 19:03:45 +0200

    Replace OpenMM Consortium list (#121)
    * Remove old consortium list
    
    * Updates to ecosystem and development pages
    
    * Text change per @tmarkland

--- components/development.vue
+++ components/development.vue
@@ -48,6 +48,19 @@
     </v-container>
     <v-container>
       <v-card flat>
+        <v-card-title>Collaborations</v-card-title>
+        <v-card-text>
+          We are consistently working with groups ranging from academic
+          researchers to industry to improve and extend the OpenMM ecosystem. If
+          you have features or improvement suggestions, please contact us via
+          our <a href="https://github.com/openmm/openmm/discussions">discussion forum</a>
+          or reach out to a member of the OpenMM Development team at the
+          Markland Lab (see above).
+        </v-card-text>
+      </v-card>
+    </v-container>
+    <v-container>
+      <v-card flat>
         <v-card-title>Funding Sources</v-card-title>
       </v-card>
     </v-container>
@@ -83,29 +96,5 @@
         </v-card-text>
       </v-card>
     </v-container>
-    <v-container>
-      <v-card flat>
-        <v-card-title>OpenMM Consortium</v-card-title>
-        <v-card-text>
-          A community of researchers and developers supporting OpenMM.
-        </v-card-text>
-      </v-card>
-    </v-container>
-    <v-container class="d-flex flex-wrap justify-center">
-      <v-card v-for="person of people.consortium" :key="person" :href="person.url" target="blank" class="ma-2" width="300" hover>
-        <v-card-text>{{ person.name }}</v-card-text>
-      </v-card>
-    </v-container>
   </div>
 </template>
-
-<script>
-module.exports = {
-  data: function() { return {
-    people: []
-  }},
-  created: function() {
-    fetch('data/people.json').then(res => res.json()).then(people => this.people = people)
-  }
-}
-</script>
--- components/ecosystem.vue
+++ components/ecosystem.vue
@@ -2,9 +2,78 @@
   <div>
     <v-container>
       <v-card flat>
+        <v-card-title>OpenMM Ecosystem</v-card-title>
+        <v-card-text>
+          These packages and tools, maintained along with the core OpenMM
+          library by the OpenMM development team, provide additional
+          functionality for building and running simulations with OpenMM, as
+          well as plugins that expand OpenMM's capabilities by interfacing with
+          other software.
+        </v-card-text>
+      </v-card>
+    </v-container>
+    <v-container class="d-flex flex-wrap justify-center">
+      <v-card href="https://github.com/openmm/openmmforcefields/" target="blank" class="ma-4" width="300" hover>
+        <v-card-title>openmmforcefields</v-card-title>
+        <v-card-text>
+          OpenMM support for AMBER, CHARMM, OpenFF, and Espaloma force fields, and small molecule parameterization with GAFF, Espaloma and the OpenFF Toolkit.
+        </v-card-text>
+      </v-card>
+      <v-card href="https://github.com/openmm/openmm-ml/" target="blank" class="ma-4" width="300" hover>
+        <v-card-title>OpenMM-ML</v-card-title>
+        <v-card-text>
+          A high-level API for using machine learning models, including ANI-1ccx, ANI-2x, and MACE-OFF23, in OpenMM simulations.
+        </v-card-text>
+      </v-card>
+      <v-card href="https://github.com/openmm/openmm-plumed/" target="blank" class="ma-4" width="300" hover>
+        <v-card-title>OpenMM-PLUMED</v-card-title>
+        <v-card-text>
+          Provides a connection between OpenMM and PLUMED. It allows you to bias or analyze an OpenMM simulation based on collective variables.
+        </v-card-text>
+      </v-card>
+      <v-card href="https://github.com/openmm/openmm-setup" target="blank" class="ma-4" width="300" hover>
+        <v-card-title>OpenMM Setup</v-card-title>
+        <v-card-text>
+          A graphical application to set up and run simulations with OpenMM. Includes pull-down menus, pop-up tips, error checking, and live script building
+        </v-card-text>
+      </v-card>
+      <v-card href="https://github.com/openmm/openmm-torch/" target="blank" class="ma-4" width="300" hover>
+        <v-card-title>OpenMM-Torch</v-card-title>
+        <v-card-text>
+          A plugin for OpenMM that allows PyTorch models to be used for defining an OpenMM force object.
+        </v-card-text>
+      </v-card>
+      <v-card href="https://github.com/openmm/openmm-xtb/" target="blank" class="ma-4" width="300" hover>
+        <v-card-title>OpenMM-XTB</v-card-title>
+        <v-card-text>
+          A plugin providing a connection between OpenMM and the XTB quantum chemistry package.
+        </v-card-text>
+      </v-card>
+      <v-card href="https://github.com/pandegroup/pdbfixer/" target="blank" class="ma-4" width="300" hover>
+        <v-card-title>PDBFixer</v-card-title>
+        <v-card-text>
+          A powerful, flexible tool for preparing PDB files for molecular simulation or modeling, capable of running in interactive, Python-scriptable, or fully automated modes.
+        </v-card-text>
+      </v-card>
+      <v-card href="https://raw.githubusercontent.com/openmm/openmm/refs/heads/master/examples/extras/optimizepme.py" target="blank" class="ma-4" width="300" hover>
+        <v-card-title>PME Optimizer</v-card-title>
+        <v-card-text>
+          A script to optimize the performance of PME. It runs a series of simulations and selects the parameters that give best performance for simulating a particular system on particular hardware.
+        </v-card-text>
+      </v-card>
+      <v-card href="https://raw.githubusercontent.com/openmm/openmm/refs/heads/master/examples/extras/rigid.py" target="blank" class="ma-4" width="300" hover>
+        <v-card-title>Rigid Body Creator</v-card-title>
+        <v-card-text>
+          A script implementing rigid bodies in OpenMM by adding appropriate constraints and converting particles to virtual sites.
+        </v-card-text>
+      </v-card>
+    </v-container>
+    <v-container>
+      <v-card flat>
         <v-card-title>Built with OpenMM</v-card-title>
         <v-card-text>
-          Trusted and used by scientists around the world.
+          Third-party packages that are not developed directly by the OpenMM
+          team, but use OpenMM or have interfaces with OpenMM.
         </v-card-text>
       </v-card>
     </v-container>
@@ -47,10 +116,28 @@
           A distributed computing infrastructure devoted to biomedical research to understand the function of biomolecules for health and disease.
         </v-card-text>
       </v-card>
-      <v-card href="https://github.com/pandegroup/pdbfixer/" target="blank" class="ma-4" width="300" hover>
-        <v-card-title>PDBFixer</v-card-title>
+      <v-card href="https://openforcefield.org/software" target="blank" class="ma-4" width="300" hover>
+        <v-card-title>Open Force Field</v-card-title>
         <v-card-text>
-          A powerful, flexible tool for preparing PDB files for molecular simulation or modeling, capable of running in interactive, Python-scriptable, or fully automated modes.
+          Tools for parameterizing, benchmarking, and using high-accuracy small molecule and biomolecular force fields.
+        </v-card-text>
+      </v-card>
+      <v-card href="https://openfree.energy/projects" target="blank" class="ma-4" width="300" hover>
+        <v-card-title>Open Free Energy</v-card-title>
+        <v-card-text>
+          Tools for performing alchemical free energy calculations.
+        </v-card-text>
+      </v-card>
+      <v-card href="https://github.com/leeping/OpenMM-MD/" target="blank" class="ma-4" width="300" hover>
+        <v-card-title>OpenMM-MD</v-card-title>
+        <v-card-text>
+           A general purpose script (with lots of comments) that uses the OpenMM Python module to drive MD simulations. Simulation options are handled via an input file.
+        </v-card-text>
+      </v-card>
+      <v-card href="https://openmmtools.readthedocs.io/" target="blank" class="ma-4" width="300" hover>
+        <v-card-title>OpenMMTools</v-card-title>
+        <v-card-text>
+          Integrators, enhanced sampling methods, test systems, and alchemical factories for OpenMM.
         </v-card-text>
       </v-card>
       <v-card href="https://siremol.org/" target="blank" class="ma-4" width="300" hover>
@@ -70,78 +157,6 @@
         <v-card-title>wepy</v-card-title>
         <v-card-text>
           A Weighted Ensemble (WE) simulation framework in Python with first class support for OpenMM.
-        </v-card-text>
-      </v-card>
-    </v-container>
-    <v-container>
-      <v-card flat>
-        <v-card-title>OpenMM Tools and Plugins</v-card-title>
-        <v-card-text>
-          Resources that make it easier to use OpenMM, and plugins that expand
-          OpenMM capabilities by interfacing with other software. The plugins
-          are developed and released independently from OpenMM.
-        </v-card-text>
-      </v-card>
-    </v-container>
-    <v-container class="d-flex flex-wrap justify-center">
-      <v-card href="https://github.com/openmm/openmmforcefields/" target="blank" class="ma-4" width="300" hover>
-        <v-card-title>openmmforcefields</v-card-title>
-        <v-card-text>
-          OpenMM support for AMBER, CHARMM, OpenFF, and Espaloma force fields, and small molecule parameterization with GAFF, Espaloma and the OpenFF Toolkit.
-        </v-card-text>
-      </v-card>
-      <v-card href="https://github.com/leeping/OpenMM-MD/" target="blank" class="ma-4" width="300" hover>
-        <v-card-title>OpenMM-MD</v-card-title>
-        <v-card-text>
-           A general purpose script (with lots of comments) that uses the OpenMM Python module to drive MD simulations. Simulation options are handled via an input file.
-        </v-card-text>
-      </v-card>
-      <v-card href="https://github.com/openmm/openmm-ml/" target="blank" class="ma-4" width="300" hover>
-        <v-card-title>OpenMM-ML</v-card-title>
-        <v-card-text>
-          A high-level API for using machine learning models, including ANI-1ccx, ANI-2x, and MACE-OFF23, in OpenMM simulations.
-        </v-card-text>
-      </v-card>
-      <v-card href="https://github.com/openmm/openmm-plumed/" target="blank" class="ma-4" width="300" hover>
-        <v-card-title>OpenMM-PLUMED</v-card-title>
-        <v-card-text>
-          Provides a connection between OpenMM and PLUMED. It allows you to bias or analyze an OpenMM simulation based on collective variables.
-        </v-card-text>
-      </v-card>
-      <v-card href="https://github.com/openmm/openmm-setup" target="blank" class="ma-4" width="300" hover>
-        <v-card-title>OpenMM Setup</v-card-title>
-        <v-card-text>
-          A graphical application to set up and run simulations with OpenMM. Includes pull-down menus, pop-up tips, error checking, and live script building
-        </v-card-text>
-      </v-card>
-      <v-card href="https://openmmtools.readthedocs.io/" target="blank" class="ma-4" width="300" hover>
-        <v-card-title>OpenMMTools</v-card-title>
-        <v-card-text>
-          Integrators, enhanced sampling methods, test systems, and alchemical factories for OpenMM.
-        </v-card-text>
-      </v-card>
-      <v-card href="https://github.com/openmm/openmm-torch/" target="blank" class="ma-4" width="300" hover>
-        <v-card-title>OpenMM-Torch</v-card-title>
-        <v-card-text>
-          A plugin for OpenMM that allows PyTorch models to be used for defining an OpenMM force object.
-        </v-card-text>
-      </v-card>
-      <v-card href="https://github.com/openmm/openmm-xtb/" target="blank" class="ma-4" width="300" hover>
-        <v-card-title>OpenMM-XTB</v-card-title>
-        <v-card-text>
-          A plugin providing a connection between OpenMM and the XTB quantum chemistry package.
-        </v-card-text>
-      </v-card>
-      <v-card href="https://raw.githubusercontent.com/openmm/openmm/refs/heads/master/examples/extras/optimizepme.py" target="blank" class="ma-4" width="300" hover>
-        <v-card-title>PME Optimizer</v-card-title>
-        <v-card-text>
-          A script to optimize the performance of PME. It runs a series of simulations and selects the parameters that give best performance for simulating a particular system on particular hardware.
-        </v-card-text>
-      </v-card>
-      <v-card href="https://raw.githubusercontent.com/openmm/openmm/refs/heads/master/examples/extras/rigid.py" target="blank" class="ma-4" width="300" hover>
-        <v-card-title>Rigid Body Creator</v-card-title>
-        <v-card-text>
-          A script implementing rigid bodies in OpenMM by adding appropriate constraints and converting particles to virtual sites.
         </v-card-text>
       </v-card>
     </v-container>
